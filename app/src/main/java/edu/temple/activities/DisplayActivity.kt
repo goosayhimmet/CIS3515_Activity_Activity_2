32cd0deee54cccbd9932efa8b818af6af14e5b45
goosayhimmet <goosay.himmet@gmail.com>
2025-02-20 12:19:24 -0500

    Added launcher

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,9 +1,12 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
 
 class DisplayActivity : AppCompatActivity() {
 
@@ -13,13 +16,34 @@ class DisplayActivity : AppCompatActivity() {
 
     private lateinit var lyricsDisplayTextView: TextView
     private lateinit var textSizeSelectorButton: Button
+    var KEY_SIZE: Int = 16
 
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            it ->
+        if(it.resultCode == RESULT_OK){
+            it.data?.apply{
+                KEY_SIZE = getIntExtra("selectedTextSize", 16)
+            }
+            lyricsDisplayTextView.setTextSize(KEY_SIZE.toFloat())
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
+
+
+
+        textSizeSelectorButton.setOnClickListener{
+            val launchIntent = Intent(this, TextSizeActivity::class.java)
+
+            launcher.launch(launchIntent)
+
+        }
+
+
 
     }
 }
